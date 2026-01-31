df8a112cfc2f8d5ceaff3ccba297669705d1c083
Jonathan Gideon Lamidjan <lamidjan.jonathan@gmail.com>
2026-01-31 10:01:13 +0700

    Fix variable name typo in scanf for Hy

--- HONDA/HONDA.cpp
+++ HONDA/HONDA.cpp
@@ -1,4 +1,4 @@
-#include <cstudio>
+#include <cstdio>
 #include <cmath>
 using namespace std;
 
@@ -7,7 +7,7 @@ int main() {
   double Hx, Hy, Kx, Ky;
   scanf("%d", &cases);
   for(int no=1; no<=cases; no++) {
-    scanf("%lf %lf %lf %lf", &Hx, &HY, &Kx, &Ky);
+    scanf("%lf %lf %lf %lf", &Hx, &Hy, &Kx, &Ky);
     printf("Case %d: %.6lf\n", no, sqrt((Hx+Kx)*(Hx+Kx) + (Hy+Ky)*(Hy+Ky))); //sqrt((Hx+Kx)^2 + (Hy+Ky)^2)
   }
   return 0;
