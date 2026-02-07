5096105da67da968230eaeb34f80521865e91334
Jonathan Gideon Lamidjan <lamidjan.jonathan@gmail.com>
2026-02-07 09:26:02 +0700

    Implement main function for MONONUM program

---
+++ MONONUM/MONONUM.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+int main() {
+	
+	int t, n;
+	scanf("%d", &t);
+	while(t--) {
+		scanf("%d", &n);
+		double val = 1.0;
+		for(int i=1; i<9; i++) {
+			val = val * (double)(n+i);
+		}
+		double result = (((double)n+9.0)/9.0 - (40320.0/val));
+		printf("%.6lf\n",result);
+	}
+
+	return 0;
+}
